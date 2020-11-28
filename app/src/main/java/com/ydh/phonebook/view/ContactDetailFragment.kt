735e6faf9ba06a25ea452e8fdf214b9980cfd4ee
PC-2 <setyajiyudha299@gmail.com>
2020-11-28 15:10:09 +0700

    detail data displayed

--- app/src/main/java/com/ydh/phonebook/view/ContactDetailFragment.kt
+++ app/src/main/java/com/ydh/phonebook/view/ContactDetailFragment.kt
@@ -5,10 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 import com.ydh.phonebook.R
+import com.ydh.phonebook.databinding.FragmentContactDetailBinding
+import com.ydh.phonebook.databinding.FragmentContactListBinding
+import com.ydh.phonebook.model.ContactModel
 
 
 class ContactDetailFragment : Fragment() {
+    lateinit var binding : FragmentContactDetailBinding
 
 
     override fun onCreateView(
@@ -16,14 +23,40 @@ class ContactDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+        binding = FragmentContactDetailBinding.inflate(inflater, container, false)
 
-        arguments?.let {
-            val args = ContactDetailFragmentArgs.fromBundle(it)
-            val contact = args.contact
-            println("Safe Argument Received=${contact}")
-        }
-        return inflater.inflate(R.layout.fragment_contact_detail, container, false)
 
+
+
+        setView()
+
+        return binding.root
     }
+
+    private fun setView(){
+        binding.apply {
+
+            arguments?.let {
+                val args = ContactDetailFragmentArgs.fromBundle(it)
+                binding.contact = args.contact
+            }
+        }
+    }
+
+    companion object{
+        @JvmStatic
+        @BindingAdapter("contactImage")
+        fun loadImage(view: ImageView, url: String?){
+            var imageUrl = url
+            if (imageUrl.isNullOrEmpty()){
+                imageUrl = "https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png"
+            }
+            Glide.with(view.context)
+                .load(imageUrl)
+                .into(view)
+        }
+    }
+
+
 
 }
